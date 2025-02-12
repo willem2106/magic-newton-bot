31eba866cd1cf9e33547a6b7f7f41301de6407be
Odipus <109467219+Odipus40@users.noreply.github.com>
2025-02-13 03:05:05 +0700

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,18 @@
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function displayHeader() {
+  process.stdout.write('\x1Bc');
+  console.log('========================================'.cyan);
+  console.log('=        Heyelsa Daily Login Bot       ='.cyan);
+  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
+  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('========================================'.cyan);
+  console.log();
+}
+
+module.exports = {
+  delay,
+  displayHeader,
+};
